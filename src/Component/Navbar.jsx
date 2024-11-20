daaa4efb7540f46c8354cc56c7c867840932c9fb
Sakib Fakir <fakirsakib22232>
2024-11-21 01:24:45 +0600

    nav bar remake

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -34,202 +34,61 @@ function Navbar() {
   const homeloc = useLocation().pathname;
   const home = useNavigate();
 
+
+  const links = <>
+  <li>   <NavLink to={'/'}>Home</NavLink></li>
+  <li>   <NavLink to={'/startLearning'}> Start Learning</NavLink></li>
+
+   <li><NavLink to={'/tutorials'}>Toutorails</NavLink></li>
+   <li><NavLink to={'/aboutus'}> About Us</NavLink></li>
+   <li><NavLink to={`/myprofile`}>Profile</NavLink></li>
+  
+  </>
+
+
+
+
   return (
-    <div className=" ">
+    <div className="px-2 rounded ">
 
-      <section className="flex  justify-between mx-auto m  border-2  rounded-md bg-gradient-to-r from-gray-600 to-gray-600 text-white px-2">
-        <div className="mt-2 cursor-pointer " onClick={()=> home('/')}>
-
-          <img src='./b.png' className="md:w-20 w-12  h-10 md:h-20"/>
-
-
-
-        </div>
-
-        <div className="list-none  space-x-2 p-2 md:visible invisible mt-4">
-          <NavLink
-            to="/"
-            className={`${
-              homeloc === "/" && "bg-black text-white p-2 rounded-md"
-            }`}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/startLearning"
-            className={`${
-              homeloc === "/startLearning" &&
-              "bg-black p-2 text-white rounded-md"
-            }`}
-          >
-            start-learning
-          </NavLink>
-          <NavLink
-            to="/tutorials"
-            className={`${
-              homeloc === "/tutorials" && "bg-black p-2 text-white rounded-md"
-            }`}
-          >
-            Tutorials
-          </NavLink>
-
-          <NavLink
-            to="aboutus"
-            className={`${
-              homeloc === "/aboutus" && "bg-black p-2 text-white rounded-md"
-            }`}
-          >
-            about-us
-          </NavLink>
-
-          <NavLink
-            to="myprofile"
-            className={`${
-              homeloc === "/myprofile" && "bg-black p-2 text-white rounded-md"
-            }`}
-          >
-            {" "}
-            my-profile
-          </NavLink>
-        </div>
-
-        <div className="p-4  gap-4  ">
-          <div className="text-black visible md:invisible text-xl md:mr-0  ">
-            <div className="dropdown " id="menu2">
-              <div
-                tabIndex={0}
-                onClick={ToggleMenu}
-                role="button"
-                className="btn btn-ghost  btn-circle"
+      <div class="navbar bg-base-100 border">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-10 w-10 "
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h7"
-                  />
-                </svg>
-              </div>
-
-              <ul
-                id="menu"
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-4 shadow md: m-0  "
-              >
-                <li>
-                  <NavLink
-                    to="/"
-                    className={`${
-                      homeloc === "/" && "bg-black text-white p-2 rounded-md"
-                    }`}
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/startlearning"
-                    className={`${
-                      homeloc === "/startlearning" &&
-                      "bg-black p-2 text-white rounded-md"
-                    }`}
-                  >
-                    start learning
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/tutorials"
-                    className={`${
-                      homeloc === "/tutorials" &&
-                      "bg-black p-2 text-white rounded-md"
-                    }`}
-                  >
-                    Tutorials
-                  </NavLink>
-                </li>
-                <li>
-                  <a>About</a>
-                </li>
-                <li>
-                  <NavLink
-                    to="aboutus"
-                    className={`${
-                      homeloc === "/aboutus" &&
-                      "bg-black p-2 text-white rounded-md"
-                    }`}
-                  >
-                    about-us
-                  </NavLink>
-                </li>
-
-                <li>
-                  <NavLink
-                    to="myprofile"
-                    className={`${
-                      homeloc === "/myprofile" &&
-                      "bg-black p-2 text-white rounded-md"
-                    }`}
-                  >
-                    {" "}
-                    my-profile
-                  </NavLink>
-                </li>
-                <li
-                  onClick={ToggleMenu}
-                  className="cursor-pointer px-3 border "
-                >
-                  Close
-                </li>
-                <div>
-                  {user ? (
-                    <div>
-                      <button
-                        onClick={handelSignOut}
-                        className=" rounded px-4 border"
-                      >
-                        Log out
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="border">
-                      <NavLink to="auth/login" className={`border `}>
-                        {" "}
-                        Log in
-                      </NavLink>
-                    </div>
-                  )}
-                </div>
-              </ul>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
+            <ul
+              tabindex="0"
+              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
           </div>
-          {/* logic */}
-
-          {user ? (
-            <div className="md:visible invisible -mt-8">
-              <button
-                onClick={handelSignOut}
-                className="px-8 py-1 border -mt-10 rounded-md"
-              >
-                Log out
-              </button>
-            </div>
-          ) : (
-            <div>
-              <NavLink to="auth/login" className={`md:visible invisible`}>
-                Log in
-              </NavLink>
-            </div>
-          )}
+          <a class="btn btn-ghost text-xl">daisyUI</a>
         </div>
-      </section>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
+            {links}
+
+          </ul>
+        </div>
+        <div class="navbar-end">
+          <a class="btn">Button</a>
+        </div>
+      </div>
     </div>
   );
 }
